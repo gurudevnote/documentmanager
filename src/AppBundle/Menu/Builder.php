3e4823e7549260ae9983ce8fd6eb5e0afe4da71c
Huy Nguyen Huu <huuhuy@gmail.com>
2015-10-07 14:19:30 +0700

    Set active menuitem

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -7,27 +7,21 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 
 class Builder extends ContainerAware
 {
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function navbarMenu(FactoryInterface $factory, array $options)
     {
+        $authorizationChecker = $this->container->get('security.authorization_checker');
         $menu = $factory->createItem('root');
-
-        $menu->addChild('navbar-header');
-        $menu['navbar-header']->addChild('Home', array('route' => 'homepage'));
-
-        $menu->addChild('navbar-collapse');
-        $menu['navbar-collapse']->addChild('navbar-nav');
-        $menu['navbar-collapse']['navbar-nav']->addChild('Documents', array('route' => 'documents'));
-        $menu['navbar-collapse']['navbar-nav']->addChild('Folders', array('route' => 'folders'));
-
+        $menu->addChild('Home', array('route' => 'homepage'));
+        $menu->addChild('Documents', array('route' => 'documents'));
+        $menu->addChild('Folders', array('route' => 'folders'));
 
         // create another menu item
-        $menu['navbar-collapse']['navbar-nav']->addChild('About Me', array('route' => 'fos_user_profile_show'));
-        // you can also add sub level's to your menu's as follows
-        $menu['navbar-collapse']['navbar-nav']['About Me']->addChild('Edit profile', array('route' => 'fos_user_profile_edit'));
-
-        $menu['navbar-collapse']->addChild('navbar-right');
-        $menu['navbar-collapse']['navbar-right']->addChild('Administrator', array('route' => 'admin_homepage'));
-
+        if ($authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $menu->addChild('About Me', array('uri' => '#'));
+            // you can also add sub level's to your menu's as follows
+            $menu['About Me']->addChild('My profile', array('route' => 'fos_user_profile_show'));
+            $menu['About Me']->addChild('Edit profile', array('route' => 'fos_user_profile_edit'));
+        }
         return $menu;
     }
 }
