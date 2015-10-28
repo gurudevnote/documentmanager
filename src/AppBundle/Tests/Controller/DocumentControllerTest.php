c63bd87d0aae6315728047bb97c386a448d0f0d6
Huy Nguyen Huu <huuhuy@gmail.com>
2015-10-28 15:32:27 +0700

    Add test function for creating a document

---
+++ src/AppBundle/Tests/Controller/DocumentControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace AppBundle\Tests\Controller;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+class DocumentControllerTest extends WebTestCase
+{
+    private  static $client = null;
+    public static function  setUpBeforeClass()
+    {
+        self::$client = static::createClient();
+        $crawler = self::$client->request('GET', '/login');
+        //$crawler->
+        $form = $crawler->selectButton('_submit')->form();
+
+        // set some values
+        $form['_username'] = 'hnguyenhuu';
+        $form['_password'] = '123456';
+
+        // submit the form
+        $crawler = self::$client->submit($form);
+    }
+
+    public function testCreateDocument()
+    {
+        //test create folder and specify parent folder
+        $repository = $this->getContainer()->get('doctrine.orm.entity_manager')->getRepository('P5:Folder');
+        $qb = $repository->createQueryBuilder('f')
+            ->select('f.id')
+            ->where('f.name = :name')
+            ->setParameter('name', 'Drawing' );
+        $folderId = $qb->getQuery()->getSingleScalarResult();
+        $crawler = self::$client->request('GET', '/documents');
+        $form = $crawler->selectButton('form[save]')->form();
+        $documentName = 'test_document_create_successful';
+        // set some values
+        $form['form[filename]'] = $documentName;
+        $form['form[type]'] = 'PDF';
+        $form['form[folder]'] = $folderId;
+
+        // submit the form
+        self::$client->submit($form);
+
+
+        self::$client->request('GET', '/documents');
+        $this->assertContains(
+            $documentName,
+            self::$client->getResponse()->getContent()
+        );
+    }
+}
